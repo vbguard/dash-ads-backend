e597fd9dd7d98d09c2d6c8ec7f5a400b92bf6433
Viktor Bulvarenko <silken.ua@gmail.com>
2019-09-17 15:34:13 +0300

    fix env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const config = require('./config/config');
 const connectToDB = require(`./config/mongodb`);
 
 const PORT = config.PORT;
-const DATABASE_URL = config.mongoURI;
+const DATABASE_URL = config.mongoURL;
 
 startServer(PORT);
 connectToDB(DATABASE_URL);
